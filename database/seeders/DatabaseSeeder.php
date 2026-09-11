450dd471039b8feafd3a4cb99059c38cb013e28a
anila-mightywarner <dev2.kochi@mightywarner.com>
2026-09-11 12:18:08 +0530

    landing page updation

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use CMS\SiteManager\Database\Seeders\CmsRolesPermissionsSeeder;
+use CMS\SiteManager\Database\Seeders\MetadataSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +19,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        $this->call([
+            CmsRolesPermissionsSeeder::class,
+            MetadataSeeder::class,
+            FilterSeeder::class,
+            PlanSeeder::class
         ]);
     }
 }
